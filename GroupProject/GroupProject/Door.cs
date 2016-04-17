cad23ad8bcc39c3e22ce46865d40c5d587e7d214
Nikolas Whiteside <nrw6218@rit.edu>
2016-04-17 13:42:25 -0400

    0.008.011 | Created a door class that inherits from Entity. Its fields include Boolean isLocked, Texturee2D tex and Vector2 position. It has a method that will turn isLocked to false and make the texture null when a door is unlocked.

---
+++ GroupProject/GroupProject/Door.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GroupProject
+{
+    class Door : Entity
+    {
+        //Fields
+        private Texture2D tex;
+        private Boolean isLocked;
+        private Vector2 position;
+
+        //Properties
+        public Boolean IsLocked
+        {
+            get { return isLocked; }
+            set { isLocked = value; }
+        }
+
+        //Constructor
+        public Door(Rectangle rect, Texture2D tex, Vector2 position)
+            :base(rect)
+        {
+            this.tex = tex;
+            this.isLocked = true;
+            this.position = position;
+        }
+
+        //Methods
+        public void Unlock()
+        {
+            this.tex = null;
+            this.isLocked = false;
+        }
+    }
+}
